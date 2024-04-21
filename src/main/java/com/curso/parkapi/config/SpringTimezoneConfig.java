fa367b4183905a926d33770c655b1c50742f77f3
Malu Martins <martinss.mmalu@gmail.com>
2024-04-21 00:42:07 -0300

    feat: criar entidade usuario

--- src/main/java/com/curso/parkapi/config/SpringTimezoneConfig.java
+++ src/main/java/com/curso/parkapi/config/SpringTimezoneConfig.java
@@ -4,8 +4,12 @@ import java.util.TimeZone;
 
 import org.springframework.context.annotation.Configuration;
 
+import jakarta.annotation.PostConstruct;
+
 @Configuration
 public class SpringTimezoneConfig {
+	
+	@PostConstruct
 	public void timezoneConfig() {
 		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
 	}
